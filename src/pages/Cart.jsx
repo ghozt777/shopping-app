d4d9aabc14a240a0e2269781dad96bb740751753
ghozt777 <ghozt777.code@gmail.com>
2021-08-08 06:27:40 +0530

    style: added style to cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,7 @@
 import {useProducts} from '../components/ProductsProvider'
 import {useUsers} from '../components/UsersProvider'
+import {FiMinus,FiTrash} from 'react-icons/fi'
+import '../styles/Product.css'
 
 export const Cart = () => {
     
@@ -7,33 +9,46 @@ export const Cart = () => {
     const {users,active,setUsers} = useUsers()
     const currUser = users.find(user => user.username===active)
 
+    function genButton(item){
+        return(
+            <div className='AddToCart'>
+                <button className = 'AddToCart btn' onClick = {() => {
+                    setUsers({type:'REDUCE_FROM_CART',payload:{user:currUser,product:item}})
+                    setUsers({type:'SET_TOTAL',payload:{user:currUser}})
+                    setProducts({type:'ADD',payload:item.id})
+                    }}> <FiMinus /> </button>
+                <button className = 'AddToCart btn' onClick = {() => {
+                    setUsers({type:'REMOVE_FROM_CART',payload:{user:currUser,product:item}})
+                    setUsers({type:'SET_TOTAL',payload:{user:currUser}})
+                    setProducts({type:'ADD_ALL',payload:item.id})
+                    }}> <FiTrash/> Delete </button>
+            </div>
+        )
+    }
+
     function genPage(){
         return currUser.cart ? (
             <>
                 <h1> Cart </h1>
-            <ul>
+            <div className='Products'>
             {
                 currUser.cart.map(item => {
                     return item.quantity>0 ? (
-                        <li>
-                            {item.name}  {item.price}  {item.quantity} 
-                            <button onClick = {() => {
-                                setUsers({type:'REDUCE_FROM_CART',payload:{user:currUser,product:item}})
-                                setUsers({type:'SET_TOTAL',payload:{user:currUser}})
-                                setProducts({type:'ADD',payload:item.id})
-                                }}> - </button>
-                            <button onClick = {() => {
-                                setUsers({type:'REMOVE_FROM_CART',payload:{user:currUser,product:item}})
-                                setUsers({type:'SET_TOTAL',payload:{user:currUser}})
-                                setProducts({type:'ADD_ALL',payload:item.id})
-                                }}> Delete Item </button>
-                        </li>
+                        <div className='Product__List'>
+                        <div className='Product__Details'> {item.name} </div>
+                        <div className='Product__Details'> Price: {item.price} </div>
+                        <div className='Product__Details'> Available Quantity: <span style={{color: item.quantity>5 ? 'green' : 'red'}}>{item.quantity}</span> </div>
+                        <div className='Product__Btn'>
+                        {genButton(item)}
+                        </div>
+                    </div>
+                    
                     ) :(
                         <></>
                     )
                 })
             }
-            </ul>
+            </div>
             <h2> The Total Cost is : {currUser.total}</h2>
             </>
         ) : (
